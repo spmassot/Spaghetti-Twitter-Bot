da3a24ea672ea6850a542c4083a29c793f25d79a
Sean Massot <sean.massot@namely.com>
2017-03-13 17:58:04 -0400

    more change2

--- send_tweet.py
+++ send_tweet.py
@@ -1,3 +1,4 @@
+import re
 import tweepy
 from auth import give_me_auth as auth
 from maketweet import main as mktwt
@@ -5,7 +6,13 @@ from maketweet import main as mktwt
 def main(in_tweet):
     api = auth()
     new_twt = mktwt(in_tweet)
-    if len(new_twt) > 135:
+    url = re.compile("http.+(\s|$)")
+    try:
+        strt = url.search(new_tweet).start()
+    except:
+        strt = 0
+
+    if len(new_twt) - strt > 135:
         api.update_status(new_twt[:135])
         api.update_status(new_twt[135:])
     else:
