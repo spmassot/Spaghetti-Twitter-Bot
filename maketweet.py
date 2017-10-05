3073e26ab238e2dd5ba13ae6f50ed01bd72bad34
root <spmassot@gmail.com>
2017-10-04 21:43:07 -0400

    makes replacement slightly less aggressive

--- maketweet.py
+++ maketweet.py
@@ -1,12 +1,11 @@
 import re
 import tweepy
 from random import choice
-from itertools import cycle
 from google_language import get_entities
 from models.corpus import update_corpus
 
 def tweet_maker(the_tweet):
-    foods = cycle((
+    foods = (
         'spaghetti',
         'meatballs',
         'marinara sauce',
@@ -18,16 +17,14 @@ def tweet_maker(the_tweet):
         'mozzarella',
         'bolognese',
         'parmesean'
-    ))
+    )
     the_new_tweet = the_tweet.text
     entities = get_entities(the_tweet.text)
     for word, entity_type in entities.items():
         update_corpus(word, entity_type)
         the_new_tweet = the_new_tweet.replace(
-            word,
-            choice(
-                feed_me(next(foods), entity_type)
-        ))
+            word, choice(feed_me(choice(foods), word, entity_type))
+        )
     return replace_url(the_new_tweet)
 
 def replace_url(in_string):
@@ -37,40 +34,37 @@ def replace_url(in_string):
         in_string
     )
 
-def feed_me(subject, entity):
+def feed_me(food, origin, entity):
     return {
         'UNKNOWN':[
-            f'{subject}',
-            f'some kind of {subject}',
+            origin,
         ],
         'PERSON':[
-            f'{subject} man',
+            f'{food} man',
         ],
         'LOCATION':[
-            f'{subject} land',
-            f'{subject} city',
+            f'{food} land',
+            f'{food} city',
         ],
         'ORGANIZATION':[
-            f'{subject.title()} Conservancy',
-            f'Department of {subject.title()}',
-            f'{subject.title()} Association',
+            f'{food.title()} Conservancy',
+            f'Department of {food.title()}',
+            f'{food.title()} Association',
         ],
         'EVENT':[
-            f'{subject.title()} Day',
-            f'war of {subject.title()}',
+            f'{food.title()} Day',
+            f'war of {food.title()}',
         ],
         'WORK_OF_ART':[
-            f'Mona {subject.title()}',
-            f'Thinking {subject.title()}',
-            f'{subject.title()} Descending a Staircase',
+            origin,
         ],
         'CONSUMER_GOOD':[
-            f'{subject}',
+            f'{food}',
         ],
         'OTHER':[
-            f'{subject}',
+            origin,
         ],
-    }.get(entity, f'{subject}')
+    }.get(entity, f'{food}')
 
 if __name__ == '__main__':
     print(tweet_maker(input('Enter some text:')))
