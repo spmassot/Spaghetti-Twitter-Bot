46d2034803ac7a250a91eab4a4fce35f61f6e431
root <spmassot@gmail.com>
2018-01-14 22:39:59 -0500

    ads a apsce

--- send_tweet.py
+++ send_tweet.py
@@ -19,7 +19,7 @@ def main(in_tweet):
         api.update_status(new_twt)
 
 if __name__ == '__main__':
-    text= 'Making America safe is my number one priority.'
+    text = 'Making America safe is my number one priority.'
     'We will not admit those into our country we cannot safely vet.'
     print(main(text))
 
