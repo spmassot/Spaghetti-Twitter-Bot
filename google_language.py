80cf44063dfa64d8c26ac9329e2580e2730823d4
spmassot <sean.massot@namely.com>
2018-08-20 11:50:49 -0400

    space

--- google_language.py
+++ google_language.py
@@ -12,4 +12,4 @@ def get_entities(in_text):
     language_client = language.Client()
     document = language_client.document_from_text(in_text)
     response = document.analyze_entities()
-    return {ent.name:ent.entity_type for ent in response.entities}
+    return {ent.name: ent.entity_type for ent in response.entities}
